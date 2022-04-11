b9e5a1da9e5badf31863d241a45a50849804ca22
chinmayd7 <87563656+chinmayd7@users.noreply.github.com>
2022-04-11 11:39:08 +0530

    Delete Lab1 directory

--- Lab1/basicEuclid.py
+++
@@ -1,7 +0,0 @@
-def gcd(a, b):
-	if a == 0:
-		return b
-	return gcd(b % a, a)
-a = int(input('Enter the larger of the two numbers'))
-b = int(input('Enter the other number'))
-print('The GCD of the numbers is : ', gcd(a, b))
